cce528306468757b4b594cae7e623ab6d5af450b
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-11 16:21:31 -0500

    feat(exercises): add solution for challenge_jurassic_park using functions and for loop
    - Implemented solution to `challenge_jurassic_park` with functions and a for loop
    - Added inline explanation inside the function about its purpose
    - Documented the expected input type and clarified the process of function creation

--- 04_logic/02_challenge_jurassic_park.py
+++ 04_logic/02_challenge_jurassic_park.py
@@ -6,3 +6,18 @@ Importante: Solo se consideran los huevos de los dinosaurios carnívoros (T-Rex)
 Objetivo:
 Escribe una función en Python que reciba una lista de números enteros y devuelva la suma total de los huevos que pertenecen a los dinosaurios carnívoros (es decir, la suma de todos los números pares en la lista).
 """
+
+def count_carnivore_dinosaur_egg(egg_list) -> int:
+    '''
+    Esta función recibe una lista de números enteros que representa la cantidad de huevos que han puesto diferentes dinosaurios en el parque jurásico y los de número par son de carnívoros. Devuelve un número con la sumna de todos los huevos de carnívoros.
+    '''
+    total_carnivore_eggs = 0
+
+    for eggs in egg_list:
+        if eggs % 2 == 0:
+            total_carnivore_eggs += eggs
+    
+    return total_carnivore_eggs
+
+egg_list = [3, 4, 7, 5, 8]
+print(count_carnivore_dinosaur_egg(egg_list))
