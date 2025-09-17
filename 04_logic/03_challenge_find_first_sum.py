5d19b0e5f5f7a4119ee64abb45e6ff77dada6c45
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-17 18:17:22 -0500

    feat(challenges): solved find first sum challenge using nested loops
    - Implemented solution with nested loops
    - Left open the alternative for a better approach

--- 04_logic/03_challenge_find_first_sum.py
+++ 04_logic/03_challenge_find_first_sum.py
@@ -6,3 +6,14 @@ goal = 8
 
 find_first_sum(nums, goal)  # [2, 3]
 """
+
+def find_first_sum(nums, goal):
+    for i in nums:
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == goal:
+                return [i, j]
+            
+nums = [4, 5, 6, 2]
+goal = 8
+find_first_sum(nums, goal)
+print(find_first_sum(nums, goal))
