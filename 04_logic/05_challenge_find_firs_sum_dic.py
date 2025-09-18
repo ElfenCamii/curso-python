c90332f649c0295c0ef6e4b48e199781e8119e3a
Camiilo Piinto <camiilo.piinto@gmail.com>
2025-09-18 13:16:23 -0500

    feat(challenges): solved find first sum challenge using dictionary
    - Implemented solution with dictionary to save time using more memory

---
+++ 04_logic/05_challenge_find_firs_sum_dic.py
@@ -0,0 +1,23 @@
+"""
+Dado un array de números y un número goal, encuentra los dos primeros números del array que sumen el número goal y devuelve sus índices. Si no existe tal combinación, devuelve None.
+
+nums = [4, 5, 6, 2]
+goal = 8
+
+find_first_sum(nums, goal)  # [2, 3]
+"""
+
+def find_first_sum(nums, goal):
+    seen = {}   # Diccionario para guardar el numero y su indice
+
+    for index, value in enumerate(nums):
+        missing = goal - value
+        if missing in seen: return [seen[missing], index]
+        seen[value] = index
+    return None
+
+nums = [4, 5, 6, 2]
+goal = 8
+
+result = find_first_sum(nums, goal)
+print(result)
