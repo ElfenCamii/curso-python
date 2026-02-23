9a63a3f25d9daa918264202e5d5542c53d00ac9d
Camiilo Piinto <camiilo.piinto@gmail.com>
2026-02-23 08:13:20 -0500

    Retomando curso de python, volviendo a ver las bases para empezar

--- 01_basic/02_types.py
+++ 01_basic/02_types.py
@@ -5,34 +5,35 @@
 ###
 
 print("int")
-# print(type(3))
-# print(type(-3))
-# print(type(0))
-# print(type(12345678901234567890))
+print(type(3))
+print(type(-3))
+print(type(0))
+print(type(12345678901234567890))
 
 print("float")
-# print(type(3.14))
-# print(type(-3.14))
-# print(type(0.0))
-# print(type(1.0e10)) # notacion cientifica
+print(type(3.14))
+print(type(-3.14))
+print(type(0.0))
+print(type(1.0e10)) # notacion cientifica
 
 print("complex:")
-# print(type(3 + 4j))
+print(type(3 + 4j))
 
 print("str:")
-# print(type("hola"))
-# print(type(' '))
-# print(type('123'))
-# print(type("""
-#            hola
-#            mundo
-#            """))
+print(type("hola"))
+print(type(' '))
+print(type('123'))
+print(type("""
+           hola
+           mundo
+           """))
 
 print("bool:")
-# print(type(True)) 
-# print(type(False))
-# print(type(3 > 2))
-# print(type(3 == 2))
+print(type(True)) 
+print(type(False))
+print(type(3 > 2))
+print(type(3 == 2))
 
 print("nonetype:")
-# print(type(None))
+print(type(None))
+
