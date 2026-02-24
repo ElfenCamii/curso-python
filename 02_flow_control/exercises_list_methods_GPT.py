6fb1ab500c2bbafbb783daef1a59323226ea0734
Camiilo Piinto <camiilo.piinto@gmail.com>
2026-02-24 17:27:58 -0500

    add last exercises of list with methods

--- 02_flow_control/exercises_list_methods_GPT.py
+++ 02_flow_control/exercises_list_methods_GPT.py
@@ -67,3 +67,94 @@ if user_name in names:
     print(f'El nombre esta en la posición {names.index(user_name)}')
 else:
     print('El nombre no esta en la lista')
+
+# 🧠 6️⃣ Contar repeticiones
+# numeros = [1, 2, 3, 2, 4, 2, 5]
+
+# Cuenta cuántas veces aparece el número 2 usando count().
+
+numeros = [1, 2, 3, 2, 4, 2, 5]
+repetido = numeros.count(2)
+
+print('El numero de veces que se repite el numero 2 es: ', repetido, 'veces')
+
+# 🧠 7️⃣ Ordenar lista
+# numeros = [8, 3, 12, 1, 7]
+
+# Ordénala de menor a mayor usando sort().
+# Luego imprímela.
+
+lista_numeros = [8, 3, 12, 1, 7]
+lista_ordeenada = sorted(lista_numeros)
+
+print('La lista original es: ', lista_numeros)
+print('La lista ordeenada es:', lista_ordeenada)
+
+lista_numeros.sort()
+print('La lista original ordenada: ', lista_numeros)
+
+# 🧠 8️⃣ Ordenar al revés
+
+# Usa la misma lista anterior.
+# Ordénala de mayor a menor sin usar sorted().
+# (Pista: sort() + algo más).
+
+lista_numeros = [8, 3, 12, 1, 7]
+lista_numeros.sort(reverse=True)
+
+print('La lista original ordenada: ', lista_numeros)
+
+# 🧠 9️⃣ Copiar lista
+# original = [1, 2, 3]
+
+# Crea una copia usando copy().
+# Agrega un número a la copia.
+# Imprime ambas listas y observa qué pasa.
+
+original = [1, 2, 3]
+copia = original.copy()
+copia.append(4)
+
+print('Lista original: ', original)
+print('Copia de la lista original: ',copia)
+
+# 🧠 🔟 BONUS – Mini sistema interactivo 🔥
+
+# Crea un pequeño menú:
+# Agregar número
+# Eliminar número
+# Mostrar lista
+# Salir
+
+# Usa:
+# append()
+# remove()
+# pop()
+
+# 👉 Mantén el programa simple (sin while si aún no lo has visto, puedes simularlo con varias ejecuciones).
+
+list_numbers = [3, 5, 12, 53, 1, 0]
+show_list = input('Quieres ver la lista? si/no: ').lower()
+
+if show_list == 'si':
+    print('La lista es: ', list_numbers)
+
+add_number = input('Quieres agregar un numero? si/no: ').lower()
+
+if add_number == 'si':
+    number_user_add = int(input('Escriba el numero que quiere agregar: '))
+    list_numbers.append(number_user_add)
+    print('La nueva lista es: ', list_numbers)
+
+delet_numbeer = input('Quieeres eliminar un numero de la lista? si/no: ').lower()
+
+if delet_numbeer == 'si':
+    number_user_delet = int(input('Escriba el numero que quieree eliminar: '))
+    list_numbers.remove(number_user_delet)
+    print('La nueva lista es: ', list_numbers)
+
+pop_number = input('Quieres eliminar el ultimo numero de la lista? si/no: ')
+
+if pop_number == 'si':
+    list_numbers.pop()
+    print('La nueva lista es: ', list_numbers)
