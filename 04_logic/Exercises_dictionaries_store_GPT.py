a9487d617d85665445230e9c6baf8950aeb0f11c
Camiilo Piinto <camiilo.piinto@gmail.com>
2026-03-08 15:07:56 -0500

    feat: add exercises store

---
+++ 04_logic/Exercises_dictionaries_store_GPT.py
@@ -0,0 +1,74 @@
+import os
+os.system('cls')
+
+# 🔥 Ahora el ejercicio completo
+
+# Este ya mezcla varias cosas que has estado practicando.
+
+# Usa este mismo diccionario:
+
+# tienda = {
+#     "frutas": {"manzana": 3000, "pera": 2500, "banana": 1800},
+#     "verduras": {"zanahoria": 2000, "tomate": 2800, "cebolla": 1500},
+#     "lacteos": {"leche": 3500, "queso": 5000}
+# }
+# Problema
+
+# Crea un menú que permita al usuario hacer lo siguiente:
+
+# 1. Ver todas las categorias
+# 2. Ver productos de una categoria
+# 3. Buscar precio de un producto
+# 4. Salir
+
+tienda = {
+    "frutas": {"manzana": 3000, "pera": 2500, "banana": 1800},
+    "verduras": {"zanahoria": 2000, "tomate": 2800, "cebolla": 1500},
+    "lacteos": {"leche": 3500, "queso": 5000, 'kefir': 4300}
+}
+
+print('-----------------------------------------')
+print('-- Bienvenidos a la tienda de Camiilo --')
+print('-----------------------------------------')
+
+while True:
+    print('\nEscriba el numero de la siguiente lista de posibilidades')
+    print('''
+1. Ver todas las categorias
+2. Ver productos de una categoria
+3. Buscar precio de un producto
+4. Salir
+''')
+    
+    try:
+        user = int(input('Escriba la opción que desee: '))
+        if user == 4:
+            print('\nGracias por visitar la tienda de Camiilo')
+            print('---------- Hasta la próxima! ----------')
+            print('-----------------------------------------')
+            break
+        elif user == 1:
+            print('\nLas categorias existentes son: \n')
+            for categorias in tienda.keys():
+                print(categorias)
+        elif user == 2:
+            user_categoria = input('\nIngrese la categoria de la que quiere saber los productos: ')
+            print(f'\nIngresaste categoria → {user_categoria}\n')
+            if tienda.get(user_categoria):
+                for buscar_categoria, precio in tienda[user_categoria].items():
+                    print(f'{buscar_categoria} : {precio}')
+            else: 
+                print(tienda.get(user_categoria, 'Categoria no encontrada!'))
+        elif user == 3:
+            user_producto = input('\nEscriba el producto del que quiere saber el precio: ')
+            print(f'\nIngresaste producto → {user_producto}\n')
+            encontrado = False
+            for buscar_pruducto in tienda.values():
+                if buscar_pruducto.get(user_producto):
+                    print(f'El producto tiene un precio de: {buscar_pruducto[user_producto]}')
+                    encontrado = True
+            if not encontrado:
+                print(buscar_pruducto.get(user_producto, 'Producto no encontrado'))
+    except ValueError:
+        print('\nEl valor ingresado no es un número!!')
+        print('Vuelve a intestarlo \n')
